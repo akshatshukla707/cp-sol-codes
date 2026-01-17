946fca9ff661a402f5e5abb25f850182c368c08b
akshatshukla707 <akshatshuklalive@gmail.com>
2026-01-17 19:17:26 +0530

    bfs implementation

--- graph/bfs/nakanj_spoj.cpp
+++ graph/bfs/nakanj_spoj.cpp
@@ -4,7 +4,9 @@
 #include<queue>
 #include<utility>
 #include<algorithm>
-
+using namespace std;
+void process();
+bool bfs(int &i , int &j , int u , int v , queue<pair<int,int>> &que , pair<int,int> &pairE , vector<vector<int>> &grid);
 int main()
 {
     int t;
@@ -28,10 +30,14 @@ void process()
     
     pair<int,int> curr,pairE;
     curr.first  = (int)strS[0]%97;
-    curr.second = (int)strs[1]%49;
+    curr.second = (int)strS[1]%49;
     
     pairE.first  = (int)strE[0]%97;
     pairE.second = (int)strE[1]%49;
+    
+    
+    
+    if(curr==pairE){cout<<0<<"\n";return;}
     
     queue<pair<int,int>> que;
     que.push(curr);
@@ -43,8 +49,13 @@ void process()
     
     int steps = 0;
     
-    while(!que.empty)
-    {
+    while(!que.empty())
+    { 
+        int c = que.size();
+        
+        while(c!=0)
+        {
+            
         int u,v;
         
         u = que.front().first;
@@ -52,17 +63,26 @@ void process()
         
         que.pop();
         
-        for(int lv = 0 ; lv<8 ; i++)
+        for(int lv=0 ; lv<8 ; lv++)
         {
-           ans = bfs(dir[lv][0],dir[lv][1],u,v,que,pairE,grid,steps);
+           ans = bfs(dir[lv][0],dir[lv][1],u,v,que,pairE,grid);
            
            if(ans==true) break;
         }
         
         
         if(ans==true) break;
-    }
+        
+         
+         c--;  
+        }
+        
+        
+        steps++;
+        
+        if(ans==true) break;
     
+    }
     
     cout<<steps<<"\n";
     
@@ -72,17 +92,16 @@ void process()
 }
 
 
-bool bfs(int &i , int &j , int u , int v queue<pair<int,int>> &que , pair<int,int> &pairE , vector<vector<int>> &grid, int &steps)
+bool bfs(int &i , int &j , int u , int v , queue<pair<int,int>> &que , pair<int,int> &pairE , vector<vector<int>> &grid)
 {
     u = u+i;
     v = v+j;
     
-    if(u<0 || v<0 || grid[u][v] == 1) return false;
+    if(u<0 || v<0 || u>7 || v>7 || grid[u][v] == 1) return false;
     
     if(u==pairE.first && v==pairE.second)
     {
         grid[u][v]=1;
-        steps++;
         return true;
     }
     
@@ -90,7 +109,123 @@ bool bfs(int &i , int &j , int u , int v queue<pair<int,int>> &que , pair<int,in
     
     que.push(curr);
     grid[u][v]=1;
-    steps++;
+    
+    return false;
+    
+    
+}
+
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<utility>
+#include<algorithm>
+using namespace std;
+void process();
+bool bfs(int &i , int &j , int u , int v , queue<pair<int,int>> &que , pair<int,int> &pairE , vector<vector<int>> &grid);
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t!=0)
+    {
+        process();
+        t--;
+    }
+
+    
+}
+
+void process()
+{
+    vector<vector<int>> grid(8,vector<int>(8,-1));
+    string strS , strE;
+    cin>>strS>>strE;
+    
+    
+    pair<int,int> curr,pairE;
+    curr.first  = (int)strS[0]%97;
+    curr.second = (int)strS[1]%49;
+    
+    pairE.first  = (int)strE[0]%97;
+    pairE.second = (int)strE[1]%49;
+    
+    
+    
+    if(curr==pairE){cout<<0<<"\n";return;}
+    
+    queue<pair<int,int>> que;
+    que.push(curr);
+    grid[curr.first][curr.second] = 1;
+    
+    vector<vector<int>> dir = {{1,2},{-1,2},{1,-2},{-1,-2},{2,1},{-2,1},{2,-1},{-2,-1}};
+    
+    bool ans = false;
+    
+    int steps = 0;
+    
+    while(!que.empty())
+    { 
+        int c = que.size();
+        
+        while(c!=0)
+        {
+            
+        int u,v;
+        
+        u = que.front().first;
+        v = que.front().second;
+        
+        que.pop();
+        
+        for(int lv=0 ; lv<8 ; lv++)
+        {
+           ans = bfs(dir[lv][0],dir[lv][1],u,v,que,pairE,grid);
+           
+           if(ans==true) break;
+        }
+        
+        
+        if(ans==true) break;
+        
+         
+         c--;  
+        }
+        
+        
+        steps++;
+        
+        if(ans==true) break;
+    
+    }
+    
+    cout<<steps<<"\n";
+    
+    
+    
+    
+}
+
+
+bool bfs(int &i , int &j , int u , int v , queue<pair<int,int>> &que , pair<int,int> &pairE , vector<vector<int>> &grid)
+{
+    u = u+i;
+    v = v+j;
+    
+    if(u<0 || v<0 || u>7 || v>7 || grid[u][v] == 1) return false;
+    
+    if(u==pairE.first && v==pairE.second)
+    {
+        grid[u][v]=1;
+        return true;
+    }
+    
+    pair<int,int> curr(u,v);
+    
+    que.push(curr);
+    grid[u][v]=1;
     
     return false;
     
