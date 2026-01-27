33006035fc7ca07d129a988d0ca504f6d82d917e
akshatshukla707 <akshatshuklalive@gmail.com>
2026-01-27 17:15:41 +0530

    Refactor Dijkstra's code structure and include headers

--- djikstra/code.cpp
+++ djikstra/code.cpp
@@ -1,10 +1,22 @@
 #include<iostream>
-#include<queue>
-#include<functional>
 #include<vector>
+#include<queue>
 #include<pair>
+#include<utility>
+#include<functional>
 
 int main()
 {
+    int n , m , u , v , d;
+    cin>>n>>m;
+    vector<vector<pair<int,int>>> grid(n+1);
     
+    for(int i=0 ; i<m ; i++)
+    {
+        cin>>u>>v>>d;
+        grid[u].push_back({d,v});
+        grid[v].push_back({d,u});
+    }
+    
+    vector<vector<int>> check(n+1,vector<int>(n+1,-1));
 }
