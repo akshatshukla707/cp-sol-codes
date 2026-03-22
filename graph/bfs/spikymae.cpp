dfa9d5618919cdc521513575807cee3d24b88354
akshatshukla707 <akshatshuklalive@gmail.com>
2026-03-22 12:30:54 +0530

    Refactor BFS function and update input handling

--- graph/bfs/spikymae.cpp
+++ graph/bfs/spikymae.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
 #include<vector>
+#include<utility>
 
+using namespace std;
+int fun(int i , int j , vector<vector<char>> &grid , int n , int m );
 int main()
 {
-    int n , m , j;
+    int n , m , s;
     
-    cin>>n>>m>>j;
+    cin>>n>>m>>s;
     pair<int,int> start;
     vector<vector<char>> grid(n,vector<char>(m));
     
@@ -23,54 +26,73 @@ int main()
         }
     }
     
-    fun(start.first , start.second , grid);
+    int obs = fun(start.first , start.second , grid , n , m);
+    
+    if(2*obs <= s)
+    {
+        cout<<"true";
+    }
+    else
+    {
+        cout<<"false";
+        
+    }
+    
 }
 
 
-fun(int i , int j , vector<vector<char>> &grid)
+int fun(int i , int j , vector<vector<char>> &grid, int n , int m)
 {
     vector<pair<int,int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
     int obs=0;
     
     while(true)
     {
+        int f = 0; 
         for(int p =0 ; p<4 ; p++)
         {
-            if( (grid[p].first+i <0) || (grid[p].second + j <0) ) continue;
+            if( (dir[p].first+i <0) || (dir[p].second + j <0) || grid[dir[p].first+i][dir[p].second+j] == '/' || (dir[p].second + j ==m) || (dir[p].second + i == n) ) continue;
             
-            if(grid[grid[p].first][grid[p].second] == '#') continue;
+            if(grid[dir[p].first+i][dir[p].second+j] == '#') continue;
             
-            if(grid[grid[p].first][grid[p].second] == 's')
+            if(grid[dir[p].first+i][dir[p].second+j] == 's')
             {
+              grid[dir[p].first+i][dir[p].second+j] = '/';
+              
+              i= dir[p].first +i;
+              j= dir[p].second+j;
               obs++;
-              continue;
-            }
-            
-            if(grid[grid[p].first][grid[p].second] == '.')
-            {
-              i= grid[p].first;
-              j= grid[p].second;
               
               break;
             }
+            else
+            {
+             if(grid[dir[p].first+i][dir[p].second+j] == '.')
+             {
+              
+              grid[dir[p].first+i][dir[p].second+j] = '/';
+              i= dir[p].first+i;
+              j= dir[p].second+j;
+              
+              break;
+              
+             }
+             else
+             {
+                f=1;
+                break;
+             }
              
+            }             
+       
+          
+      
         }
+        
+        if( f==1 ) break;
+        
     }
     
+    
+    return obs;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
